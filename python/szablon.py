62f9fe9a9421c33f91464a2e6a76cfbe965b8c95
mmucha123 <michalmucha2001@interia.eu>
2018-09-11 12:21:07 +0200

    dodanie szablonu python

---
+++ python/szablon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+
+
+
+
+def main(args):
+    print("Witaj!")
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
