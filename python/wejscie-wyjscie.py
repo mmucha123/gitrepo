23adae21e2616e90a424416d1edf05e95ca756eb
mmucha123 <michalmucha2001@interia.eu>
2018-09-12 08:45:37 +0200

    początek

---
+++ python/wejscie-wyjscie.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+
+
+
+
+def main(args):
+    a = input ("podaj 1. liczbę: ")
+    b = input ("podaj 2 liczbę: ")
+    print(a)
+    print(b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
