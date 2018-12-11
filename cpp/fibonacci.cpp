93fc4ba4fd749bec22c6cce419a45ae5529b5cd0
mmucha123 <michalmucha2001@interia.eu>
2018-12-11 12:35:37 +0100

    fibonacii.re

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -26,6 +26,26 @@ long int fibonacci_it(int n){
     return wynik;
 }
 
+long int fibonacci_re(int n){
+    
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci_re(n-1) + fibonacci_re(n-2);
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
 int main(int argc, char **argv)
 {
 	int n;
---
+++ cpp/silnia.cpp
@@ -0,0 +1,32 @@
+/*
+ * silnia.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int silnia_re(int n){
+    if(n == 0) 
+        return 1;
+  return silnia_re(n-1) * n;
+    
+    
+    }
+
+
+int main(int argc, char **argv)
+{   
+    int n = 10;
+    
+    
+    cout << silnia_re(n) << endl;
+    
+	
+	return 0;
+}
+
